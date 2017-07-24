f46234a77c20816dbe8f88ae958574d61cf2c3cd
zerojay <jay@zerojay.com>
2017-07-24 02:03:35 -0400

    Minor update for system creation.

--- scriptmodules/ports/omxplayer.sh
+++ scriptmodules/ports/omxplayer.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-
 # This file is part of The RetroPie Project
-# 
-# The RetroPie Project is the legal property of its developers, whose names are
-# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
-# 
-# See the LICENSE.md file at the top-level directory of this distribution and 
-# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
-
+# The RetroPie Project is the legal property of its developers, whose names are too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
 rp_module_id="omxplayer"
 rp_module_desc="omxplayer - Video Player"
 rp_module_help="Put your videos into $"
@@ -19,9 +15,8 @@ rp_module_flags="!mali !x86"
 function install_bin_omxplayer() {
     aptInstall omxplayer
 }
-
 function configure_omxplayer() {
     mkRomDir "videos"
-    addEmulator 1 "$md_id" "Videos" "omxplayer %ROM%"
-    addSystem "$md_id" "Videos" ".mp4 .MP4 .mov .MOV .avi .AVI .mkv .MKV"
+    addEmulator 0 "$md_id" "videos" "omxplayer %ROM%"
+    addSystem "videos" "Videos" ".mp4 .MP4 .mov .MOV .avi .AVI .mkv .MKV"
 }
